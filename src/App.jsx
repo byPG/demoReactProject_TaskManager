dd12783295db31e0125c7a84dc2036a9431867d2
Paulina <pg.gastol@gmail.com>
2026-02-25 19:00:48 +0100

    logic and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
+import { useState } from "react";
+
 import YourProjectsSidebar from "./components/YourProjectsSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+
+      return{
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+let content;
+
+if(projectsState.selectedProjectId === null){
+  content = <NewProject onAdd={handleAddProject}/> 
+}else if (projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <YourProjectsSidebar/>
-      <NewProject/>
+      <YourProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
     </main>
   );
 }
